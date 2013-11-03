8a06e0c7f1b5fd8d92db1b2259274c4fee0e0a12
Mark Saroufim <Mark@Marks-MacBook-Pro-4.local>
2013-11-03 00:53:57 -0700

    uploaded skeleton for gshare, still need to fix shift and define table sizes in the predictor.h file

--- src/predictor.C
+++ src/predictor.C
@@ -1,36 +1,76 @@
 // A very stupid predictor.  It will always predict not taken.
+#include <math.h>
+
+#define HBSIZE 256
 
 void init_predictor () 
 {
-int LHT[LLHT];            //Local history table.
-int GHT[LGHR];            //Global history table
-int Bias[SATBITS];        //Bias 
+
+//history buffer and pattern history table
+
+int PHTSIZE = pow(HBSIZE,2)-1;
+int PHT[PHTSIZE]; 
+int HB[HBSIZE];
 
 
-/*
-by default we will initalize our history as all branches taken
-this assumption is fair since for loops are the most common
-conditional branches and the ones that are the most likely taken
-*/
-for(int i =0; i < LLHT; i++) {
-	LHT[i] = 1;
+//initialize History buffer to be all not taken
+for(int i =0; i < HBSIZE; i++) {
+	HB[i] = 0;
 }
 
-for(int i =0; i < LGHR; i++) {
-	LGHR[i] = 1;
-}
-
-for(int i =0; i < SATBITS; i++) {
-	Bias[i] = 1;
+//initialize Pattern History table to strongly not taken
+for(int i = 0; i < PHTSIZE; i++ ) {
+	PHT[i] = 0;
 }
 
 }
+
 
  bool make_prediction (unsigned int pc)
 {
-  return false;
+  /*
+  0 strongly not taken
+  1 weakly not taken
+  2 weakly taken
+  3 strongly taken
+  */
+  if (PHT[pc] > 1) {
+  	return true;
+  }
+  else {
+  	return false;
+  }
 }
 
 void train_predictor (unsigned int pc, bool outcome)
 {
+	//still need to a find a way to shift the outcome into the rightmost part of the HB
+	PHT[HB[index]];
+	index = pc ^ HB;
+
+
+	//take care of saturing counters
+	if(PHT[index] == 3 && outcome == 1) {
+		return;
+	}
+
+	else if(PHT[index] == 0 && outcome == 0) {
+		return;
+	}
+
+
+	else if(outcome == 1) {
+		PHT[index]++;
+		return;
+	}
+
+	else if(outcome == 0) {
+		PHT[index]--;
+		return;
+	}
+
+	else {
+		return;
+	}
+
 }
