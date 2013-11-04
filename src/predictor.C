777cc3446a55ddba3e22661a295e27c49db08cd8
Mark Saroufim <Mark@Marks-MacBook-Pro-4.local>
2013-11-04 09:43:42 -0800

    tried changing history buffer to just an unsigned integer, getting crazy compiler warning though unrecognized x86

--- src/predictor.C
+++ src/predictor.C
@@ -1,19 +1,21 @@
 #include <math.h>
 
-#define HBSIZE 256
-#define PHTSIZE 65536 //2^HBSIZE - 1
+//size of history buffer
+#define HBSIZE 32
+
+//size of Pattern history table
+#define PHTSIZE 4294967295 //2^HBSIZE - 1
 
 extern unsigned int PHT[PHTSIZE]; 
-extern unsigned int HB[HBSIZE];
+
+extern unsigned int HB;
 
 void init_predictor () 
 
 {
 
 //initialize History buffer to be all not taken
-for(int i =0; i < HBSIZE; i++) {
-	HB[i] = 0;
-}
+HB = 0;
 
 //initialize Pattern History table to strongly not taken
 for(int i = 0; i < PHTSIZE; i++ ) {
@@ -42,21 +44,21 @@ for(int i = 0; i < PHTSIZE; i++ ) {
 void train_predictor (unsigned int pc, bool outcome)
 {
 	//cant xor an unsigned array of size 256 with a pc need to fix this issue
-	index = pc ^ HB;
+	unsigned int index = pc ^ HB;
 	
 
 	//update HB when new outcome is observed
 	//take care of saturing counters
+	//left shift, add a 0 in the end by default
 	if(PHT[index] == 3 && outcome == 1) {
 		HB = HB << 1;
-		HB[HBSIZE] = 1;
+		HB++;
 		return;
 
 	}
 
 	else if(PHT[index] == 0 && outcome == 0) {
 		HB = HB << 1;
-		HB[HBSIZE] = 0;
 		return;
 	}
 
@@ -64,7 +66,7 @@ void train_predictor (unsigned int pc, bool outcome)
 	else if(outcome == 1) {
 		PHT[index]++;
 		HB = HB << 1;
-		HB[HBSIZE] = 1;
+		HB++;
 		return;
 	}
 
